98a8bc461bfd8c3c7d03bc0a9091ee2480348197
balendu9 <balendurawat9@gmail.com>
2025-07-25 00:37:29 +0530

    stream

--- main.py
+++ main.py
@@ -3,25 +3,21 @@ import subprocess
 import time
 from threading import Thread
 from flask import Flask
-from dotenv import load_dotenv
-
-load_dotenv()
 
 STREAM_URL = os.getenv("YOUTUBE_STREAM_URL")
 STREAM_KEY = os.getenv("YOUTUBE_STREAM_KEY")
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def health():
     return "Lofi stream alive ✅", 200
 
+def run_flask():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 def start_stream():
     while True:
-        print("Starting 11 hour stream")
-
         command = [
             "ffmpeg",
             "-re",
@@ -36,15 +32,10 @@ def start_stream():
             f"{STREAM_URL}/{STREAM_KEY}"
         ]
 
-        try:
-            process = subprocess.Popen(command)
-            process.wait()
-            print("Stream ended or crashed. Restarting in 5s...")
-            time.sleep(5)
-        except Exception as e:
-            print(f"error: {e}")
-            time.sleep(5)
-        
+        process = subprocess.Popen(command)
+        process.wait()
+        time.sleep(5)
+
 if __name__ == "__main__":
     Thread(target=run_flask).start()
     start_stream()
